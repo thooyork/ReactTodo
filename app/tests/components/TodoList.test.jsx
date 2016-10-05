47e4ec1c120816444ba6997447e517e74d82e009
thooyork <thomas@smart-sign.com>
2016-10-05 15:53:42 +0200

    more tests

--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -5,9 +5,19 @@ var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
 
 var TodoList = require('TodoList');
+var Todo = require('Todo');
+
 
 describe('TodoList',()=>{
   it('Should exist',()=>{
     expect(TodoList).toExist();
+  });
+
+  it('Should render one Todo Compoment for each Todo Item',()=>{
+    var todos = [{id:1,text:'do something'},{id:2,text:'some data'}];
+    var todolist = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+    var todosComponents = TestUtils.scryRenderedComponentsWithType(todolist, Todo);
+
+    expect(todosComponents.length).toBe(todos.length);
   });
 });
